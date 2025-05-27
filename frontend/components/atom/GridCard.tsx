4cee9d8ae4aae76def1f3b60c24af875b8a29790
jdodiya-codal <jdodiya@codal.com>
2025-05-27 11:22:44 +0530

    minor UI update

--- frontend/components/atom/GridCard.tsx
+++ frontend/components/atom/GridCard.tsx
@@ -14,7 +14,7 @@ export default function GridCard(props: GridCardProps) {
   console.log(props)
   return (
     <Link href={`post/${props.slug}`}>
-      <div className="flex bg-white max-w-[50%] max-h-[389px] rounded-[12px] p-[10px]">
+      <div className="flex bg-white max-w-[100%] max-h-[389px] rounded-[12px] p-[10px]">
         <img
           src={urlFor(props?.mainImage).width(500).url()}
           alt=""
@@ -22,8 +22,8 @@ export default function GridCard(props: GridCardProps) {
           height={190}
           className="max-w-full max-h-[190px] rounded-[12px]"
         />
-        <div>
-          <h3 className="text-[16px] font-semibold mt-[20px]">{props.title}</h3>
+        <div className="ml-4">
+          <h3 className="text-[16px] font-semibold mt-[20px] max-w-full">{props.title}</h3>
           <p className="mt-[10px] line-clamp-2 opacity-75 text-[14px] leading-[20px]">
             {props.description ||
               'Ah, the joy of the open road—it’s a good feeling. But if you’re new to driving, you may feel a little nervous about getting behind the wheel. Don’t worry. While it’s true that accidents can happen to anybody, there are things you can do to drive safely and do your best to avoid them. '}
